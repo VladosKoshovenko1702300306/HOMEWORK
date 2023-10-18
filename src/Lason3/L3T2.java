ca40c83ee8759b4381070d63d59bf073a7554423
burvinkl <chikattillo@gmail.com>
2023-10-18 21:11:47 +0300

    ain

--- src/Lason3/L3T2.java
+++ src/Lason3/L3T2.java
@@ -1,2 +1,20 @@
-package Lason3;public class L3T2 {
+package Lason3;
+import java.util.Scanner;
+public class L3T2 {
+    public static void main (String [] ags) {
+        System.out.println("Введіть фразу");
+        Scanner scan = new Scanner(System.in);  String a = scan.nextLine();
+        String b =(a.replaceAll("[\\s|\\u00A0]+", ""));
+        System.out.println(b);
+        System.out.println(isPalindrom (b) ? "Yes" : "No");
+
+
+    }
+private static boolean isPalindrom (String b){
+        if(b.length()==1 || b.length()==0)
+            return true;
+
+        if (b.charAt(0)==b.charAt(b.length()-1))
+            return isPalindrom(b.substring(1, b.length()-1));
+return  false;}
 }
--- src/Lason3/L3T3.java
+++ src/Lason3/L3T3.java
@@ -1,2 +1,26 @@
-package Lason3;public class L3T3 {
-}
+package Lason3;
+import java.util.Scanner;
+public class L3T3{
+    public static void main (String [] ags) {
+        System.out.println("Введіть фразу");
+        Scanner scan = new Scanner(System.in);  String a = scan.nextLine();
+        int words =0;
+        if (a.length() !=0){
+            words ++;
+            for (int i = 0; i < a.length(); i++) {
+                if (a.charAt(i)== ' '){
+                    words ++ ;
+
+                }
+            }
+
+        }
+        System.out.println("Ви ввели таку кількість слів"+" "+ (words));
+
+
+
+
+
+
+                }
+            }
