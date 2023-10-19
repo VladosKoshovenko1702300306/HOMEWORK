b66cbc969009c37479fd767a349ad88c6e9d6854
burvinkl <chikattillo@gmail.com>
2023-10-19 17:25:55 +0300

    lason 1 t4 restart

--- src/Lason1/Task4.java
+++ src/Lason1/Task4.java
@@ -2,5 +2,7 @@ public class Task4 {
     public static void main(String[] args) {
         int a = 5;
         int b = 3;
+        System.out.println("a ="+( (a*0)+b));
+        System.out.println("b ="+( (b*0)+a));
     }
 }
