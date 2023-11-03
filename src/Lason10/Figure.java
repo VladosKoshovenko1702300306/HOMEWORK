78822ad5fdde6de899e84932dd7fb0d99b042064
burvinkl <chikattillo@gmail.com>
2023-11-03 19:49:19 +0300

    Lason 10

---
+++ src/Lason10/Figure.java
@@ -0,0 +1,5 @@
+package Lason10;
+import java.util.Scanner;
+abstract class Figure {
+    public abstract double perimeter ();
+    public abstract double area (); }
--- src/Lason2/Task2.java
+++ src/Lason2/Task2.java
@@ -1,25 +1,38 @@
 package Lason2;
 import java.util.Scanner;
+
 public class Task2 {
-    public static void main(String[] agrs) {
-        Scanner point = new Scanner(System.in);
-        System.out.println("Ведіть значення х від точки 1");
-        double x1 = point.nextDouble();
-        System.out.println("Ведіть значення y від точки 1");
-        double y1 = point.nextDouble();
-        System.out.println("Ведіть значення х від точки 2");
-        double x2 = point.nextDouble();
-        System.out.println("Ведіть значення y від точки 2");
-        double y2 = point.nextDouble();
-        double ab = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)); // сторона 1
-        System.out.println("Ведіть значення х від точки 3");
-        double x3 = point.nextDouble();
-        System.out.println("Ведіть значення y від точки 3");
-        double y3 = point.nextDouble();
-        double bc = Math.sqrt(Math.pow(x2 - x3, 2) + Math.pow(y2 - y3, 2)); //сторона 2
-        double ac = Math.sqrt(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2)); // сторона 3
-        double p = (ab + ac + bc) / 2; // пів перемитер
-        double s = Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double x1 = getInput("Введіть значення x від точки 1", scanner);
+        double y1 = getInput("Введіть значення y від точки 1", scanner);
+        double x2 = getInput("Введіть значення x від точки 2", scanner);
+        double y2 = getInput("Введіть значення y від точки 2", scanner);
+
+        double ab = calculateDistance(x1, y1, x2, y2);
+
+        double x3 = getInput("Введіть значення x від точки 3", scanner);
+        double y3 = getInput("Введіть значення y від точки 3", scanner);
+
+        double bc = calculateDistance(x2, y2, x3, y3);
+        double ac = calculateDistance(x1, y1, x3, y3);
+
+        double s = calculateTriangleArea(ab, bc, ac);
         System.out.println(s);
+    }
+
+    public static double getInput(String message, Scanner scanner) {
+        System.out.println(message);
+        return scanner.nextDouble();
+    }
+
+    public static double calculateDistance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    }
+
+    public static double calculateTriangleArea(double ab, double bc, double ac) {
+        double p = (ab + ac + bc) / 2;
+        return Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
     }
 }
